cf1a53e453faa42f7f62a3a4cd72eb02c6f4719e
Manish Gehani <manish@manishs-macbook-pro.local>
2016-10-28 10:36:02 +0530

    Initial Commit

---
+++ Travellr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travellr
+//
+//  Created by Manish Gehani on 10/28/16.
+//  Copyright © 2016 Manish Gehani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
